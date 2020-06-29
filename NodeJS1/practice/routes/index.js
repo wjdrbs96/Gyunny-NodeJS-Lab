1ac805641f627f7a614d77000c8e000df21313a9
최정균 <wjdrbs966@naver.com>
2020-06-30 00:54:43 +0900

    nodejs study

--- NodeJS1/practice/routes/index.js
+++ NodeJS1/practice/routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 router.use('/post', require('./post'));
 router.use('/user', require('./user'));
 router.use('/auth', require('./auth'));
+router.use('/login', require('./login'));
 
 module.exports = router;
--- NodeJS1/practice/routes/user.js
+++ NodeJS1/practice/routes/user.js
@@ -1,66 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-const resMessage = require('../modules/responseMessage');
-const util = require('../modules/util');
-const statusCode = require('../modules/statusCode');
-const crypto = require('../modules/crypto'); 
-const jwt = require('../modules/jwt');
+const userController = require('../controllers/user');
 
-router.post('/signup', async(req, res) => {
-    const {loginId, password, name, email} = req.body;
-
-    if (!loginId || !password || !name || !email) {
-        res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
-        return;
-    }
-
-    if (await User.checkUser(loginId)) {
-        res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.ALREADY_ID));
-        return;
-    }
-
-    const {salt, hashed} = await crypto.encrypt(password);
-    const idx = await User.signUp(loginId, hashed, salt, name, email);
-    if (idx === -1) {
-        res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
-        return;
-    }
-
-    let user = [loginId, name, email];
-
-    res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.CREATED_USER, user))
-})
-
-
-router.post('/signin', async(req, res) => {
-    const {loginId, password} = req.body;
-
-    if (!loginId || !password) {
-        res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST,resMessage.NULL_VALUE));
-        return;
-    }
-
-    const user = await User.getUserById(loginId);
-    if (user.length === 0) {
-        res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NO_USER));
-        return;
-    }
-
-    const hashed = await crypto.encryptWithSalt(password, user[0].salt);
-
-    if (hashed !== user[0].password) {
-        res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.MISS_MATCH_PW));
-        return;
-    }
-    
-    const {token, _} = await jwt.sign(user[0]);
-
-    res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.LOGIN_SUCCESS, {
-        AccessToken : token
-    }));
-
-})
+router.post('/signup', userController.signup);
+router.post('/signin', userController.signin);
 
 module.exports = router;
 
