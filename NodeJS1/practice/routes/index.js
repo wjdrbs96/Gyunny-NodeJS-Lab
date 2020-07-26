7935fd5e50144bcfd88ecdad5a8470e70450004a
최정균 <choejeong-gyun@choejeong-gyun-ui-MacBookPro.local>
2020-07-26 23:15:55 +0900

    folder reSetting and NodeJS passport test

--- NodeJS1/practice/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.use('/post', require('./post'));
-router.use('/user', require('./user'));
-router.use('/auth', require('./auth'));
-router.use('/test', require('./test'));
-
-module.exports = router;
