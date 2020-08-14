b4692f1c1956be1fb2ca9877e7f605d33a4899f6
최정균 <wjdrbs966@naver.com>
2020-08-14 11:33:50 +0900

    KAKAO OPEN API SUCCESS

--- KAKAO_API/routes/auth.js
+++ KAKAO_API/routes/auth.js
@@ -1,26 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const statusCode = require('../modules/statusCode');
-const resMessage = require('../modules/responseMessage');
-const util = require('../modules/util');
 const request = require('request');
-const querystring = require('querystring')
 
 
 let kakaoOptions = {
-  uri: `https://dapi.kakao.com/v3/search/book?target=title&query=미움받을용기`,
+  uri: `https://dapi.kakao.com/v3/search/book?target=title`,
   method: 'GET',
   headers: {
     'Authorization': 'KakaoAK 856ec0be1a62b01007353103f2cbc64d'
   },
+  qs: {
+    query : '미움받을용기'
+  },
   encoding: 'UTF-8',
 }
 
-function strapiCallback(error, response, body) {
-  console.log(body);
-}
-
-request(kakaoOptions, strapiCallback)
-
+request(kakaoOptions, function (err, res, body) {
+  if (!err && res.statusCode == 200) {
+    console.log(JSON.parse(body));
+  }  
+})
 
 module.exports = router;
