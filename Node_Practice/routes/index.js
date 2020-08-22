125d8b8bd5ce04dee81683545e6d391f93ebe942
최정균 <wjdrbs966@naver.com>
2020-08-23 00:46:10 +0900

    내장 모듈 http

--- Node_Practice/routes/index.js
+++ Node_Practice/routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const http = require('http');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+  res.write('<h1>Hello Node!</h1>');
+  res.end('<p>Hello Server!</p>');
+})
+.listen(8080, () => {
+  console.log('서버 대기중');
+})
 
 router.use('/crypto', require('./crypto'));
 
