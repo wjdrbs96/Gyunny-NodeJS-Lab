6954e7517c7596aac9e43c4a9e1dd36e27119d6d
최정균 <wjdrbs966@naver.com>
2020-09-13 16:55:25 +0900

    폴더이름 변경 + 삭제

--- Node_Practice/routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.use('/crypto', require('./crypto'));
-router.use('/socket', require('./socket'));
-
-module.exports = router;
