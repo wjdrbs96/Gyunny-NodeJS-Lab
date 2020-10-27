2c4c437edc977557c47adec8dadc714906a10738
Gyunny <wjdrbs966@naver.com>
2020-10-27 22:43:28 +0900

    Kakao login success add

--- social_login_project/routes/auth.js
+++ social_login_project/routes/auth.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport')
 const KakaoStrategy = require('passport-kakao').Strategy;
-const rp = require('request-promise');
 
 passport.use('kakao', new KakaoStrategy({
     clientID: '856ec0be1a62b01007353103f2cbc64d',
     callbackURL: '/auth/kakao/callback',
   }, async (accessToken, refreshToken, profile, done) => {
-    console.log(profile);
+    //console.log(profile);
     console.log(accessToken);
     console.log(refreshToken);
 }))
